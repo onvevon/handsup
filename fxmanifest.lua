44d729f1a6d96e6e5aed158f3421dda8fed3de0d
onvevon <119787795+onvevon@users.noreply.github.com>
2024-11-01 17:53:14 +0000

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'adamant'
+games { 'gta5' };
+
+
+--[[ Resource Information ]]--
+name         'hansup'
+version      '1.0.0'
+description  'script for Handsup'
+author       'juiceezzZ'
+repository   'https://github.com/onvevon/handsup/'
+
+
+client_script 'client.lua'
